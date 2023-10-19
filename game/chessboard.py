d3d19d6acdd615f55a1013672b50345af29b82c1
jdabassett <jacobbassett@gmail.com>
2023-10-18 21:00:03 -0600

    basic setup with tests

--- game/chessboard.py
+++ game/chessboard.py
@@ -4,4 +4,15 @@ class ChessBoard():
     def __int__(self):
         pass
 
-    def add_red
+    def add_red(self):
+        pass
+
+    def add_blue(self):
+        pass
+
+    def render(self):
+        pass
+
+    def is_under_attack(self):
+        pass
+
